b03c7e232e9ea2697499fc80dc13b7de33524ac5
Emu <617398198@qq.com>
2022-01-22 17:03:29 +0800

    last commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,18 @@
 import styles from './index.less';
 import { useState } from 'react';
 import { Button } from 'antd';
+import { request } from 'umi';
 export default function IndexPage() {
   const [count, setCount] = useState(0);
   const clickCount = () => {
-    setCount(count + 1);
+    setCount(count + 1)
   };
   return (
     <div>
-      <Button style={{marginTop: 10}} type="primary" onClick={clickCount}>Click this button count</Button>
-      <p style={{marginLeft: '5px'}}>{count}</p>
+      <Button style={{ marginTop: 10 }} type="primary" onClick={clickCount}>
+        Click this button count
+      </Button>
+      <p style={{ marginLeft: '5px' }}>{count}</p>
     </div>
   );
 }
