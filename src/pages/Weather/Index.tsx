5f42464da231b345ae4c67e51987b969693178f9
Emu <617398198@qq.com>
2022-01-28 19:18:52 +0800

    添加城市编码表

--- src/pages/Weather/Index.tsx
+++ src/pages/Weather/Index.tsx
@@ -1,8 +1,24 @@
 import { useState } from 'react';
 import { Button } from 'antd';
 import styles from './index.less';
+import { request } from 'umi';
+import response from './weather.json';
 
 export default function Weather() {
-  const [num, setNum] = useState(1);
-  return <div>天气</div>;
+  const [citycode, setCitycode] = useState('010');
+  const [adcode, setAdode] = useState('110000');
+  const [city, setCity] = useState('萧山区');
+  const filterCitycode = () => {
+    // const result = response.filter((item) => item.name === adcode);
+    // console.log(result);
+    request(
+      `https://restapi.amap.com/v3/weather/weatherInfo?city=${city}&key=d4a22a87b175e73f76bde1c1cfd43913`,
+      {
+        params: {
+          extensions: 'base',
+        },
+      },
+    );
+  };
+  return <Button onClick={filterCitycode}>天气</Button>;
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import Weather from '@/pages/Weather/Index';
 export default function IndexPage() {
   return (
-    <div>
+    <div style={{ marginTop: '10px', marginLeft: '10px' }}>
       <Weather />
     </div>
   );
