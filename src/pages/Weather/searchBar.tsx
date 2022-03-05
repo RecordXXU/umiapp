4c754033608dea558a7af93b55646969e15a1d57
Emu <617398198@qq.com>
2022-03-05 18:29:41 +0800

    添加searchBar

--- src/pages/Weather/searchBar.tsx
+++ src/pages/Weather/searchBar.tsx
@@ -1,10 +1,33 @@
+import { useState } from 'react';
+import { useDispatch } from 'umi';
 import { Input, Divider } from 'antd';
 import styles from './index.less';
 const SearchBar = () => {
+  const [city, setCity] = useState('');
+  const [key, setKey] = useState('d4a22a87b175e73f76bde1c1cfd43913');
+  const dispatch = useDispatch();
+  const searchCity = (e: any) => {
+    console.log(e.target.value);
+    setCity(e.target.value);
+    // dispatch({
+    //   type: 'weather/getAddress',
+    //   payload: {
+    //     key,
+    //   },
+    //   callback: (res: any) => {
+    //     console.log(res);
+    //   },
+    // });
+  };
   return (
     <div className={styles.searchBar}>
       <div>
-        <Input bordered={false} placeholder="搜索城市" />
+        <Input
+          bordered={false}
+          placeholder="搜索城市"
+          value={city}
+          onChange={(e) => searchCity(e)}
+        />
         <Divider />
       </div>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import styles from './index.less';
 import Weather from '@/pages/Weather/Index';
 export default function IndexPage() {
   return (
-    <div>
+    <div className={styles.box}>
       <Weather />
     </div>
   );
